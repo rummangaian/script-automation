dbb2fcbaa7880fd635a7a2473376296e7485f9a9
rummangaian <mohammad.r@mobiusdtaas.ai>
2025-06-17 12:24:53 +0530

    added the get node details of figma

--- controllers/figmaController.js
+++ controllers/figmaController.js
@@ -1,15 +1,30 @@
 const figmaService = require("../services/figmaService");
+require("dotenv").config()
 
-exports.formPayload = async (req, res) => {
+const formPayload = async (req, res) => {
   try {
     const { id } = req.body;
     const result = await figmaService.figmaPayload(id);
-    // if (!result) {
-    //   return res.status(404).json({ message: "No payload found for the given ID" });
-    // }
     res.status(200).json({ data: result });
   } catch (error) {
     console.error("Failed to fetch figma payload:", error);
     res.status(500).json({ error: "Internal server error" });
   }
 };
+
+const getFigmaData = async (req , res) => {
+  try {
+    const {id , fileKey} = req.query;
+    const result = await figmaService.getFigmaNodeData(fileKey , id , process.env.FIGMA_PAT);
+    res.status(200).json({
+      message:"Success",
+      data : result
+    })
+  } catch (error) {
+    console.error("error", error);
+    res.status(500).json({
+      error : "Internal Server Error"
+    })
+  }
+}
+module.exports = {formPayload , getFigmaData}
